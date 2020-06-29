1525d118a2ec3142341b1f944502256827dd0392
Your Namepidan <737606706@qq.com>
2020-06-29 10:53:20 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "lib-flexible" ;
+import "lib-flexible";
 import './index.css';
 import App from './App';
+import "lib-flexible";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
